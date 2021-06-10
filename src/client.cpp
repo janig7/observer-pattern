23236bf8592bdc10966fc9f2de68ec71656f4b67
Kuntof <75488962+Kuntof@users.noreply.github.com>
2021-06-10 20:42:12 +0200

    Update client.cpp

--- src/client.cpp
+++ src/client.cpp
@@ -5,7 +5,13 @@ Client::Client(WeatherStation &weatherstation) : weatherstation_(weatherstation)
     this->weatherstation_.Add(this);
     std::cout<<"Client created!";
 }
-
+/*
+Metoda informująca o nowych warunkach pogodowych 
+@param temperature zmiennoprzecinkowa liczba oznaczająca temperature
+@param pressure zmiennoprzecinkowa liczba okreslajaca cisnienie
+@param humidity zmiennoprzecinkowa liczba określająca wilgotnosc
+@return void 
+*/
 void Client::Update(float temperature, float pressure, float humidity) {
     std::cout << "New temperature: " << temperature << ", pressure: " << pressure << ", humidity: " << humidity << std::endl;
 }
