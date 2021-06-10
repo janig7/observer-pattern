8ac24dba03823e6c5da0a973f3f8ca1ae50d9072
mkazan93 <85441186+mkazan93@users.noreply.github.com>
2021-06-10 23:16:04 +0200

    update docstrings in weatherstation.h

--- include/weatherstation.h
+++ include/weatherstation.h
@@ -4,34 +4,37 @@
 #include <iostream>
 #include <list>
 
-// Klasa stacja pogodowa implementująca interfejs IObservable 
+///
+/// Klasa stacja pogodowa implementująca interfejs IObservable 
+///
+
 class WeatherStation : public IObservable {
     public:
-        /*
+        /**
         Metoda, która dodająca obserwatora do listy obserwatorów 
         @param IObserver  obiekt obserwatora
         @return void
         */
         void Add(IObserver *observer) override;
-        /*
+        /**
         Metoda usuwająca obserwatora z listy obserwatorów.
         @param IObserver  obiekt obserwatora
         @return void
         */
         void Remove(IObserver *observer) override;
-        /*
+        /**
         Metoda informująca wszystkich obserwatorów o zmianach pogody.
         @param void
         @return void 
         */
         void Notify() override;
-        /*
+        /**
         Metoda informująca ilu jest obserwatorów stacji pogodowej
         @param void
         @return void
         */
         void HowManyObserver();
-        /*
+        /**
         Metoda ustawiająca nowe warunki pogodowe i informująca subskrybentów o zmianach
         @param temperature zmiennoprzecinkowa liczba oznaczająca temperature
         @param pressure zmiennoprzecinkowa liczba okreslajaca cisnienie
