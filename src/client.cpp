944f8ef4d8dd1e7ed114df775d8d3ddccc64d864
mkazan93 <85441186+mkazan93@users.noreply.github.com>
2021-06-10 23:24:42 +0200

    update docstring in client.cpp

--- src/client.cpp
+++ src/client.cpp
@@ -5,7 +5,7 @@ Client::Client(WeatherStation &weatherstation) : weatherstation_(weatherstation)
     this->weatherstation_.Add(this);
     std::cout<<"Client created!";
 }
-/*
+/**
 Metoda informująca o nowych warunkach pogodowych 
 @param temperature zmiennoprzecinkowa liczba oznaczająca temperature
 @param pressure zmiennoprzecinkowa liczba okreslajaca cisnienie
